399317fd72d797624ebb2eec7340785098902afa
Vili Heikkilä <vili.heikkila@solita.fi>
2021-10-03 16:21:29 +0300

    changes to mocking

--- hsl/addressSearch_test.go
+++ hsl/addressSearch_test.go
@@ -9,16 +9,21 @@ import (
 
 type MockClient struct{}
 
-func (m MockClient) Do(_ *http.Request) (*http.Response, error) {
-	body := []byte(`{"features":[{"geometry":{"type":"Point","coordinates":[24.9,60.1]}}]}`)
-	response := http.Response{
-		StatusCode: 200,
-		Body:       ioutil.NopCloser(bytes.NewReader(body)),
-	}
-	return &response, nil
+var DoFunc func(req *http.Request) (*http.Response, error)
+
+func (m MockClient) Do(req *http.Request) (*http.Response, error) {
+	return DoFunc(req)
 }
 
 func TestAddressToCoordinates(t *testing.T) {
+	DoFunc = func(req *http.Request) (*http.Response, error) {
+		body := []byte(`{"features":[{"geometry":{"type":"Point","coordinates":[24.9,60.1]}}]}`)
+		response := http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewReader(body)),
+		}
+		return &response, nil
+	}
 	address := "some address"
 	Client = MockClient{}
 	got := addressToCoordinates(address)
